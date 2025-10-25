addbd2b808dc64e77547d7041227db2fbf214b23
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:30:13 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'JACK BLACK';
+export let nameSongs = [
+	"KUNG FU FIGHTING", /*1*/
+	"PEACHES", /*2*/
+	"POLKA BUBBLES", /*3*/
+	"MORE IS MY LOVE", /*4*/
+	"OLE OLE POLKA", /*5*/
+	"LOVE LIFTED ME", /*6*/
+	"RAPPIN' POLKA", /*7*/
+	"CLARINETTER'S POLK", /*8*/
+	"COSMIC I", /*9*/
+	"CHAD GADYA", /*10*/
+	"OH HANUKKAH", /*11*/
+	"WE ALL WEAR CLOAKS", /*12*/
+	"BEAUTIFUL DREAMER", /*13*/
+	"TO BE AN AMERICAN", /*14*/
+	"SEVENTY-SIX TROMBONES", /*15*/
+	"SCHOOL OF ROCK", /*16*/
+	"HEAL ME, I'M HEARTSICK", /*17*/
+	"T.V. EYE", /*18*/
+	"IT'S A LONG WAY TO THE TOP", /*19*/
+	"FIGHT", /*20*/
+]
